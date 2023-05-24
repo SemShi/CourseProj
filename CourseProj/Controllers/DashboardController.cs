6796688011198448305e3230a895c592d57ac9ca
Семен Шишкин <shishkinsn.20@edu.ystu.ru>
2023-05-25 00:28:02 +0300

    Настроен внешний вид сайта. Подключены необходимые скрипты.

--- CourseProj/Controllers/DashboardController.cs
+++ CourseProj/Controllers/DashboardController.cs
@@ -17,6 +17,11 @@ public class DashboardController : Controller
     {
         return View();
     }
+    
+    public IActionResult About()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
